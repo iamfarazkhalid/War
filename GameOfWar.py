a0f99f1bdc32742a561d2a6238d18f405b6fc7d3
Faraz Khalid <mynameisfarazkhalid@gmail.com>
2022-01-13 00:35:07 -0600

    WarGame basic logic implemented
    Base case of winning by having no cards implemented.

--- GameOfWar.py
+++ GameOfWar.py
@@ -6,6 +6,7 @@ from random import shuffle
 def main():
     activePlayer = []
     cardsDeck = Deck()
+    gameRounds = 0
 
     firstPlayerName = str(input("Enter First Player Name: "))
     firstPlayer = ParticipatingPlayer(firstPlayerName)
@@ -20,6 +21,28 @@ def main():
     for i in range(26):
         firstPlayer.addCardToDeck(cardsDeck.popCard())
         secondPlayer.addCardToDeck(cardsDeck.popCard())
+
+    gameActiveStatus = True
+    PlayerNumberOne = current_players[0]
+    PlayerNumberTwo = current_players[1]
+
+    while gameActiveStatus:
+        gameRounds += 1
+
+        if len(PlayerNumberOne.deckOfPlayer) == 0:
+            print("{} : You have an empty deck now.\n".format(PlayerNumberOne.playerName))
+            print( "{} is the winner.\n".format(PlayerNumberTwo.playerName))
+            gameActiveStatus = False
+            break
+
+        elif len(PlayerNumberTwo.deckOfPlayer) == 0:
+            print("{} : You have an empty deck now.\n".format(PlayerNumberTwo.playerName))
+            print("{} is the winner.\n".format(PlayerNumberOne.playerName))
+            gameActiveStatus = False
+            break
+        
+        else:
+            #Implement War Game Logic
 
 if __name__ == '__main__':
     main()
